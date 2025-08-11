a02f1c74373b0f54cdfbe1e3ef427cce55f9e415
panhanhen <panhanhen@gmail.com>
2025-08-11 21:58:56 +0700

    Test 18

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get("/health", (_, res) => res.status(200).send("ok"));
 
 app.get("/", (_, res) => {
   res.json({
-    message: "Hello from ECS Fargate behind ALB (no DB/Redis)!",
+    message: "Hello from ECS Fargate behind ALB. Thank You!",
     uptime_secs: process.uptime()
   });
 });
